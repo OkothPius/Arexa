943ebc23efb6e3a913f4cff8d118b9a75040ec94
Okoth Pius <orukopius8@gmail.com>
2021-07-09 16:24:03 +0300

    feat: create models

--- backend/rental/models.py
+++ backend/rental/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Home(models.Model):
+    """
+    A model to display home attributes.
+    """
+    title = models.CharField(max_length=100)
+    image = models.ImageField()
+    price = models.PositiveIntegerField()
+    pub_date = models.DateField()
+    estate = models.CharField(max_length=100)
+
+class Category(models.Model):
+    """
+    Model for category.
+    """
+    home = models.ManyToManyField(Home)
+
+class Location(models.Model):
+    home = models.ForeignKey(
+        Home,
+        on_delete = models.CASCADE,
+    )
