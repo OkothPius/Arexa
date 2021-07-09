e794f9442ceab0b371b7e0242178ac638a7b369b
Okoth Pius <orukopius8@gmail.com>
2021-07-09 14:38:30 +0300

    feat: update

--- backend/rental/urls.py
+++ backend/rental/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HomeListView
 
 urlpatterns = [
-    path('', views.HomeListView.as_view(), name='home-list'),
+    path('', HomeListView.as_view(), name='home-list'),
 ]
