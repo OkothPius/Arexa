c18031d7876147e01b8c720fdd57aff1b1bc2f8c
Okoth Pius <orukopius8@gmail.com>
2021-07-09 16:59:23 +0300

    feat: update views

--- backend/rental/views.py
+++ backend/rental/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import ListView
+from .models import Rental
 
 
 class HomeListView(ListView):
-    template_name = 'home.html'
+
+    models = Rental
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
