42b491cee927145beb91807a865ae66d67213851
Okoth Pius <orukopius8@gmail.com>
2021-07-09 16:59:06 +0300

    fix: update

--- backend/rental/models.py
+++ backend/rental/models.py
@@ -1,24 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
-class Home(models.Model):
-    """
-    A model to display home attributes.
-    """
+class Rental(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     price = models.PositiveIntegerField()
     pub_date = models.DateField()
     estate = models.CharField(max_length=100)
 
 class Category(models.Model):
-    """
-    Model for category.
-    """
-    home = models.ManyToManyField(Home)
+    home = models.ManyToManyField(Rental)
 
 class Location(models.Model):
     home = models.ForeignKey(
-        Home,
+        Rental,
         on_delete = models.CASCADE,
     )
