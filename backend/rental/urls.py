367d82b37ee91f0c3e7532a12dfa6318de724c60
Okoth Pius <orukopius8@gmail.com>
2021-07-22 22:34:51 +0300

    feat: create detail post page

--- backend/rental/urls.py
+++ backend/rental/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeListView
+from .views import HomeListView, PostDetailView
 from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('home/', HomeListView.as_view(), name='home-list'),
+    path('rental/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
 ]
