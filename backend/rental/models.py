d16d715767cbaa8cc8c89379a0441979600dfa31
Okoth Pius <orukopius8@gmail.com>
2021-07-23 00:35:10 +0300

    feat: update

--- backend/rental/models.py
+++ backend/rental/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 class Rental(models.Model):
     title = models.CharField(max_length=100)
@@ -14,9 +15,23 @@ class Rental(models.Model):
     def __str__(self):
         return self.title
 
+    # Handles redirect
+    def get_absolute_url(self):
+        return reverse('post-detail' ,kwargs={'pk': self.pk})
+
 class Images(models.Model):
+    head = models.CharField(max_length=100, unique=True, null=True)
     post = models.ForeignKey(Rental, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='rental_posts', verbose_name='Image')
+    image = models.ImageField(upload_to='rental_posts')
+
+
+    def __str__(self):
+        return str(self.head)
+
+    class Meta:
+        verbose_name = 'Image'
+        verbose_name_plural='Images'
+
 
 class Category(models.Model):
     home = models.ManyToManyField(Rental)
