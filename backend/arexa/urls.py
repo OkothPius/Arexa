4b7f3b8e4ad96a826ba2a87a058bacf345f6b8fc
Okoth Pius <orukopius8@gmail.com>
2021-07-22 14:36:48 +0300

    feat: update

--- backend/arexa/urls.py
+++ backend/arexa/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rental/', include('rental.urls')),
+    path('', include('rental.urls')),
 ]
