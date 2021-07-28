937fa041f4efed732fb93d5ac6968abf7476adba
Okoth Pius <orukopius8@gmail.com>
2021-07-28 15:05:40 +0300

    feat: update search

--- backend/rental/urls.py
+++ backend/rental/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomeListView, PostDetailView
+from .views import HomeListView, PostDetailView, SearchView
 from . import views
+
+app_name = 'rental'
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('home/', HomeListView.as_view(), name='home-list'),
     path('rental/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
