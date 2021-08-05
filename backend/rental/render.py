ca5afd79c0255d99e94796f4f03dc4bd85041433
Okoth Pius <orukopius8@gmail.com>
2021-08-06 00:01:23 +0300

    feat: update

--- backend/rental/render.py
+++ backend/rental/render.py
@@ -13,6 +13,6 @@ class Render:
         response = BytesIO()
         pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
         if not pdf.err:
-            return HttpResponse(response.getValue(), content_type='application/pdf')
+            return HttpResponse(response.getvalue(), content_type='application/pdf')
         else:
             return HttpResponse("Error Rendering PDF", status=400)
