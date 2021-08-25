84211310faa2abe2f806bbfae532ffc88282f4e8
Okoth Pius <orukopius8@gmail.com>
2021-08-25 10:53:28 +0300

    feat: customize django

--- backend/rental/admin.py
+++ backend/rental/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Rental, Images
+from .models import Rental, Images, Location, Category
 
-admin.site.register(Rental)
+class ImageInline(admin.TabularInline):
+    model = Images
+    extra = 3
+
+class RentalAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                         {'fields': ['pub_date', 'title']}),
+        ('House Description Details' , {'fields': ['house_detail', 'price', 'estate', 'image'], 'classes': ['collapse']}),
+    ]
+
+    list_display = ('title', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['title']
+
+    inlines = [ImageInline]
+
+
+admin.site.register(Rental, RentalAdmin)
 admin.site.register(Images)
+admin.site.register(Location)
+admin.site.register(Category)
