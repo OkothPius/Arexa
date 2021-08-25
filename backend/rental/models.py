edb809956a4e7a3fbfec8c3549be029707772a21
Okoth Pius <orukopius8@gmail.com>
2021-08-25 10:53:57 +0300

    feat: update models

--- backend/rental/models.py
+++ backend/rental/models.py
@@ -1,13 +1,15 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib import admin
 
 class Rental(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
     price = models.IntegerField()
     house_detail = models.TextField(default='Full Furnished')
-    pub_date = models.DateField()
+    pub_date = models.DateTimeField('date published')
     estate = models.CharField(max_length=100)
 
     class Meta:
@@ -15,6 +17,15 @@ class Rental(models.Model):
 
     def __str__(self):
         return self.title
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     # Handles redirect
     def get_absolute_url(self):
@@ -37,8 +48,15 @@ class Images(models.Model):
 class Category(models.Model):
     home = models.ManyToManyField(Rental)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural='Categories'
+
 class Location(models.Model):
     home = models.ForeignKey(
         Rental,
         on_delete = models.CASCADE,
     )
+
+    def __str__(self):
+        return str(self.home)
