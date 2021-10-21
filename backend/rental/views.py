5a4080a1cb395ea7a4ab14bfd96f90368169895e
Okoth Pius <orukopius8@gmail.com>
2021-10-21 23:03:06 +0300

    feat: update report module view

--- backend/rental/views.py
+++ backend/rental/views.py
@@ -1,9 +1,15 @@
+import io
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.views import generic
 from django.db.models import Q
 from .models import Rental, Images
 from .render import Render
+
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 
 def home(request):
     return render(request, 'rental/base.html')
@@ -33,6 +39,7 @@ class PostDetailView(generic.DetailView):
         context['images'] = Images.objects.all()
         return context
 
+#Search view
 class SearchView(generic.TemplateView):
     template_name = 'rental/search.html'
     models = Rental
@@ -49,6 +56,39 @@ class SearchView(generic.TemplateView):
         context['results'] = results
         return context
 
+#Generate a PDF File houses List
+def report_module(request):
+    # Create ByteStream Buffer
+    buf = io.BytesIO()
+    #create a canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    # create a text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    # Add some lines of text
+    lines = [
+        "This is line 1",
+        "This is line 2",
+        "This is line 3",
+    ]
+
+    # Loop
+    for line in lines:
+        textob.textLine(line)
+
+    # Finish Up
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    # Return Something
+    return FileResponse(buf, as_attachment=True, filename='report.pdf')
+
+
+# Generating PDF file for each specific listing
 class PdfView(generic.TemplateView):
 
     def get(self, request):
