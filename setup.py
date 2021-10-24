e6cb58bb1f68d31f7917760a6fcbc11dd67584bd
Okoth Pius <orukopius8@gmail.com>
2021-10-24 22:02:32 +0300

    feat: update setups.py

--- setup.py
+++ setup.py
@@ -1,52 +1,17 @@
-from distutils.core import setup
-from setuptools import find_packages
-import os
+#!/usr/bin/env python3
 
-
-# User-friendly description from README.md
-current_directory = os.path.dirname(os.path.abspath(__file__))
-try:
-    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
-except Exception:
-    long_description = ''
+from setuptools import setup, find_packages
 
 setup(
-    # Name of the package
-    name=<name of current directory>,
-
-    # Packages to include into the distribution
-    packages=find_packages('.'),
-
-    # Start with a small number and increase it with every change you make
-    # https://semver.org
-    version='1.0.0',
-
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    # For example: MIT
-    license='',
-
-    # Short description of your library
-    description='',
-
-    # Long description of your library
-    long_description = long_description,
-    long_description_context_type = 'text/markdown',
-
-    # Your name
-    author='',
-
-    # Your email
-    author_email='',
-
-    # Either the link to your github or to your website
-    url='',
-
-    # Link from which the project can be downloaded
-    download_url='',
-
-    # List of keyword arguments
-    keywords=[],
-
-    # List of packages to install with this one
-    install_requires=[],
+    name="Arexa",
+    description="An application that offers tenant suggestions\n
+        where they should find great housing"
+    "for Django.",
+    version='3.2.5',
+    url="https://orukopius",
+    author="Oruko Pius",
+    author_email="test@user.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=("Django>=2",),
+)
